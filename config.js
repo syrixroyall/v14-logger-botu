9390328bfac9c0dbc1447b8d2437e05c7bed6f47
syrixroyall <syrixbeys@gmail.com>
2024-05-19 12:32:31 +0300

    v14-log-bot
    V14 Discord Log Botudur!

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+token: "BOT_TOKEN", //Botun Tokeni
+presence: "By Syrix", //Botun Durumu
+genellog: "İD_GİRİN", 
+kanallog: "İD_GİRİN",
+rollog: "İD_GİRİN",
+userlog: "İD_GİRİN",
+emojilog: "İD_GİRİN",
+sunuculog: "İD_GİRİN",
+mesajlog: "İD_GİRİN",
+voicelog: "İD_GİRİN",
+statSystem: true, //Sunucu Büyükse false yapın
+}
